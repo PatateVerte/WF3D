9b593fca6613ba3a54ddae6eaebf753653efb291
PATATE_VERTE <patate.verte@yahoo.com>
2020-03-22 16:41:43 +0100

    Added diagonalization

--- Include/WF3D/Geometry/matrix3x3.h
+++ Include/WF3D/Geometry/matrix3x3.h
@@ -60,10 +60,36 @@ static inline wf3d_matrix3x3* wf3d_matrix3x3_copy(wf3d_matrix3x3* M, wf3d_matrix
     return M;
 }
 
+//norm2(A)
+static inline float wf3d_matrix3x3_norm2(wf3d_matrix3x3 const* A)
+{
+    float square_norm = 0.0;
+    for(int j = 0 ; j < 3 ; j++)
+    {
+        square_norm += wf3d_vect3d_dot(A->column[j], A->column[j]);
+    }
+
+    return sqrtf(square_norm);
+}
+
+//Tr(A)
+static inline float wf3d_matrix3x3_Trace(wf3d_matrix3x3 const* A)
+{
+    return wf3d_vect3d_get_component(A->column[0], 0) +
+            wf3d_vect3d_get_component(A->column[1], 1) +
+            wf3d_vect3d_get_component(A->column[2], 2);
+}
+
 //det(A)
 static inline float wf3d_matrix3x3_det(wf3d_matrix3x3 const* A)
 {
     return wf3d_vect3d_triple(A->column[0], A->column[1], A->column[2]);
 }
+
+//A = P * D * tP with A symetric
+//A is considered invertible
+//Parameter P is optional
+//Return D
+wf3d_matrix3x3* wf3d_matrix3x3_diagonalize_sym(wf3d_matrix3x3* D, wf3d_matrix3x3* P, wf3d_matrix3x3 const* A);
 
 #endif // WF3D_MATRIX3X3_H_INCLUDED
--- src/Geometry/matrix3x3.c
+++ src/Geometry/matrix3x3.c
@@ -66,7 +66,10 @@ wf3d_vect3d wf3d_matrix3x3_transform(wf3d_matrix3x3 const* A, wf3d_vect3d v)
 
     for(int j = 0 ; j < 3 ; j++)
     {
-        wf3d_vect3d_add_scalar_mul(vr, A->column[j], flat_vect[j + 1]);
+        vr = wf3d_vect3d_add(
+                                vr,
+                                wf3d_vect3d_scalar_mul(A->column[j], flat_vect[j + 1])
+                            );
     }
 
     return vr;
@@ -77,19 +80,96 @@ wf3d_vect3d wf3d_matrix3x3_transform(wf3d_matrix3x3 const* A, wf3d_vect3d v)
 //
 wf3d_matrix3x3* wf3d_matrix3x3_mul(wf3d_matrix3x3* M, wf3d_matrix3x3 const* A, wf3d_matrix3x3 const* B)
 {
-    wf3d_matrix3x3 T;
-
-    wf3d_matrix3x3* S = (M != A) ? M : &T;
+    wf3d_matrix3x3 S;
 
     for(int j = 0 ; j < 3 ; j++)
     {
-        S->column[j] = wf3d_matrix3x3_transform(A, B->column[j]);
+        S.column[j] = wf3d_matrix3x3_transform(A, B->column[j]);
     }
 
-    if(M != S)
-    {
-        wf3d_matrix3x3_copy(M, S);
-    }
+    wf3d_matrix3x3_copy(M, &S);
 
     return M;
+}
+
+//A = P * D * tP
+//
+//
+wf3d_matrix3x3* wf3d_matrix3x3_diagonalize_sym(wf3d_matrix3x3* D, wf3d_matrix3x3* P, wf3d_matrix3x3 const* A)
+{
+    #define diag_nb_iter 25
+
+    float vp_list[3] = {0.0, 0.0, 0.0};
+
+    wf3d_matrix3x3 M;
+    wf3d_matrix3x3_copy(&M, A);
+
+    wf3d_matrix3x3 B;
+    wf3d_matrix3x3_diag(&B, 1.0);
+
+    wf3d_matrix3x3 VP_Base;
+    wf3d_matrix3x3_zero(&VP_Base);
+
+    for(int base_j = 0 ; base_j < 2 ; base_j++)
+    {
+        for(int k = 0 ; k < diag_nb_iter ; k++)
+        {
+            wf3d_matrix3x3_mul(&M, &M, &M);
+            float norm2_M =  wf3d_matrix3x3_norm2(&M);
+            wf3d_matrix3x3_scalar_mul(&M, &M, 1.0 / norm2_M);
+        }
+
+        wf3d_matrix3x3 T;
+        wf3d_matrix3x3_mul(&T, &M, &B);
+
+        int r = 0;
+        float vp_abs = 0.0;
+        for(int j = 0 ; j < 3 ; j++)
+        {
+            float tmp = wf3d_vect3d_norm( T.column[j] );
+            if(tmp >= vp_abs)
+            {
+                r = j;
+                vp_abs = tmp;
+            }
+        }
+
+        VP_Base.column[base_j] = wf3d_vect3d_normalize( T.column[r] );
+        vp_list[base_j] = wf3d_vect3d_dot(
+                                            VP_Base.column[base_j],
+                                            wf3d_matrix3x3_transform(A, VP_Base.column[base_j])
+                                          );
+
+        if(base_j == 0)
+        {
+            wf3d_matrix3x3_copy(&M, A);
+            for(int j = 0 ; j < 3 ; j++)
+            {
+                M.column[j] = wf3d_vect3d_sub(
+                                                M.column[j],
+                                                wf3d_vect3d_scalar_mul(
+                                                                        VP_Base.column[0],
+                                                                        vp_list[0] * wf3d_vect3d_dot(VP_Base.column[0], B.column[j])
+                                                                       )
+                                               );
+            }
+        }
+    }
+
+    vp_list[2] = wf3d_matrix3x3_det(A) / (vp_list[0] * vp_list[1]);
+    VP_Base.column[2] = wf3d_vect3d_cross(VP_Base.column[0], VP_Base.column[1]);
+
+    for(int j = 0 ; j < 3 ; j++)
+    {
+        D->column[j] = wf3d_vect3d_scalar_mul(B.column[j], vp_list[j]);
+    }
+
+    if(P != NULL)
+    {
+        wf3d_matrix3x3_copy(P, &VP_Base);
+    }
+
+    return D;
+
+    #undef diag_nb_iter
 }
